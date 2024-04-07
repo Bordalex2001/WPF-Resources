e25d024d0004704c5c9dc07fde47e79ebecf46a1
Bord_bf73@student.itstep.org <Bord_bf73@student.itstep.org>
2024-04-08 00:36:40 +0300

    Add project files.

---
+++ Localization (example 1)/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Resources
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void chngLngBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ResourceDictionary currentDictionary = Application.Current.Resources.MergedDictionaries[0];
+            if (currentDictionary.Source != null)
+            {
+                string currentUri = currentDictionary.Source.ToString();
+                if (currentUri.EndsWith(".en-US.xaml"))
+                {
+                    Uri changedUri = new Uri("Resources/Dictionary.uk-UA.xaml", UriKind.Relative);
+                    ResourceDictionary changedDictionary = new ResourceDictionary { Source = changedUri };
+                    Application.Current.Resources.MergedDictionaries[0] = changedDictionary;
+                }
+                else if (currentUri.EndsWith(".uk-UA.xaml"))
+                {
+                    Uri changedUri = new Uri("Resources/Dictionary.en-US.xaml", UriKind.Relative);
+                    ResourceDictionary changedDictionary = new ResourceDictionary { Source = changedUri };
+                    Application.Current.Resources.MergedDictionaries[0] = changedDictionary;
+                }
+            }
+        }
+    }
+}
---
+++ Localization (example 2)/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Reflection;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Localization__example_2_.Properties.Resources;
+using System.Threading;
+
+namespace WPF_Resources
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += UpdateUI;
+        }
+        
+        private void UpdateUI(object sender, EventArgs e)
+        {
+            mainWin.Title = Dictionary.winTitle;
+            nameLbl.Content = Dictionary.nameLbl_text;
+            surnameLbl.Content = Dictionary.surnameLbl_text;
+            phoneLbl.Content = Dictionary.phoneLbl_text;
+            okBtn.Content = Dictionary.okBtn_text;
+            cancelBtn.Content = Dictionary.cancelBtn_text;
+            chngLngBtn.Content = Dictionary.chngLngBtn_text;
+        }
+
+        private void chngLngBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CultureInfo currentCulture = Thread.CurrentThread.CurrentUICulture;
+            if (currentCulture.Name == "uk-UA")
+            {
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+            }
+            else if (currentCulture.Name == "en-US")
+            {
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("uk-UA");
+            }
+            UpdateUI(sender, e);
+        }
+    }
+}
